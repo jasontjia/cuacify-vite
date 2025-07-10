14f183a7ee237dd8661df7454ea770e7f1465b10
Jason Ciayadi <christopher.ciayadi2511@gmail.com>
2025-07-10 20:21:27 +0800

    little changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './Landing';
-import Weather from './Weather';
+import Weather from './weather';
 import './index.css'; // Pastikan Tailwind terpasang
 
 ReactDOM.createRoot(document.getElementById('root')).render(
